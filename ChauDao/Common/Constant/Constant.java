eddf683c4cd04031c3e0825d07d3dac7be8d6e6c
chaudao <chau.dao@logigear.com>
2018-11-15 17:47:27 +0700

    code

---
+++ ChauDao/Common/Constant/Constant.java
@@ -0,0 +1,10 @@
+package Constant;
+
+import org.openqa.selenium.WebDriver;
+
+public class Constant {
+	public static WebDriver WEBDRIVER;
+	public static final String RAILWAY_URL = "http://192.168.189.206:8888/";
+	public static final String USERNAM = "chau.dao@logigear.com";
+	public static final String PASSWORD = "12345678";
+}
